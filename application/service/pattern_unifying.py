eaf0ef723f80a825ddef35d873c10a0559d84c4c
Jessica Singer <singer.jeess@gmail.com>
2022-07-19 09:41:22 -0300

    delete unused code

--- application/service/pattern_unifying.py
+++ application/service/pattern_unifying.py
@@ -20,48 +20,6 @@ class PatternUnifying:
                 all_possible_assignments.append(next_list)
         return all_possible_assignments
 
-    # def compare_two(self, base_pattern: Pattern, comparison_pattern: Pattern, index_base: int, index_comp: int):
-    #     result = True
-    #     for i in range(0, base_pattern.total_nodes):
-    #         for j in range(0, comparison_pattern.total_nodes):
-    #             if can_work_as_node_i(base_pattern, comparison_pattern, i, j):
-    #
-    #
-    # def subtract(self, base_pattern: Pattern, comparison_pattern: Pattern):
-    #     start_point = 0
-    #     pattern_index = 0
-    #     for start_point in range(0, comparison_pattern.total_nodes):
-    #         if can_work_as_that_pattern_index()
-    #
-    #     return comparison_pattern
-    def this_pattern_works(self, comp_pattern: Pattern, base_pattern: Pattern, assignments):
-        i = len(assignments)
-
-        # Make sure that every edge between assigned vertices in the subgraph is also an
-        # edge in the graph.
-        for edge in base_pattern.edges:
-            if edge[0] < i and edge[1] < i:
-                if not comp_pattern.has_edge(assignments[edge[0]], assignments[edge[1]]):
-                    return False
-        for non_edge in base_pattern.non_edges:
-            if non_edge[0] < i and non_edge[1] < i:
-                if not comp_pattern.has_non_edge(assignments[non_edge[0]], assignments[non_edge[1]]):
-                    return False
-
-        # If all the vertices in the subgraph are assigned, then we are done.
-        if i == base_pattern.total_nodes:
-            return True
-
-        # Otherwise, go through all the possible assignments for the next vertex of
-        # the subgraph and try it.
-        for j in range(0, comp_pattern.total_nodes):
-            if j not in assignments:
-                assignments.append(j)
-                if self.this_pattern_works(comp_pattern, base_pattern, assignments):
-                    # This worked, so we've found an isomorphism.
-                    return True
-                assignments.pop()
-
     def subpatterns_of_pattern(self, base_pattern: Pattern, comp_pattern: Pattern):
         assignments = self.possible_assignments([i for i in range(0, comp_pattern.total_nodes)],
                                                 base_pattern.total_nodes)
